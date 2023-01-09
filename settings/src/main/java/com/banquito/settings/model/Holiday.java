15842b1d8df225da1cd9f159b1bd5e0288598210
Carvajal Patiño Luis Enrique <44406616+carvajalluisweb@users.noreply.github.com>
2023-01-09 18:38:21 -0500

    Crear holiday controller,service, model repository (#2)
    * Crear holiday controller,service, model repository
    
    * Correcion de observaciones
    
    * Location Service (#3)
    
    * Add: Location model
    
    * Add: Location Repository
    
    * Add: Location Service
    
    * Add: Location Controller
    
    * Update: Service for findByCompleteLocation
    
    * Rename repository custom method (#4)
    
    * Add: Location model
    
    * Add: Location Repository
    
    * Add: Location Service
    
    * Add: Location Controller
    
    * Update: Service for findByCompleteLocation
    
    * Rename repository custom method
    
    * Fix comments
    
    Co-authored-by: Jorge Luis Galarza Cruz <44406615+jlgalarza3@users.noreply.github.com>
    Co-authored-by: Kevinhurtado <44406663+Kevinhurtado@users.noreply.github.com>

---
+++ settings/src/main/java/com/banquito/settings/model/Holiday.java
@@ -0,0 +1,27 @@
+package com.banquito.settings.model;
+
+import java.util.Date;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Document(collection = "holiday")
+public class Holiday {
+
+    @Id
+    private Date date;
+
+    @Field(value="code_location")
+    private Integer locationCode;
+
+    @Field(value="name")
+    private String name;
+    
+    @Field(value="type")
+    private String type;
+
+}
