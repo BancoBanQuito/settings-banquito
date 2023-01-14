932182bb6ed1f47cd953ca0dcb73d9fc995f7380
Kevin Joseph Duy Hurtado <44406663+kjduy@users.noreply.github.com>
2023-01-13 19:01:29 -0500

    Kd branch (#7)
    * Add branch model, repository, service and controller
    
    * Add a new model to store the oficce hours, dtos (RS, RQ) and mapper. Modified the branch model, repository, service and controller

--- settings/src/main/java/com/banquito/settings/service/BranchService.java
+++ settings/src/main/java/com/banquito/settings/service/BranchService.java
@@ -17,27 +17,55 @@ public class BranchService {
         this.branchRepository = branchRepository;
     }
 
-    public List<Branch> findAll(){
+    public Iterable<Branch> findAll() {
         return branchRepository.findAll();
     }
 
-    public List<Branch> findByNameLike(String name){
+    public List<Branch> findByNameLike(String name) {
         return branchRepository.findByNameLike(name);
     }
 
     @Transactional
-    public void createBranch(Branch branch){
-        this.branchRepository.save(branch);
+    public void createBranch(Branch branch) {
+        createOrUpdateBranch(branch);
     }
 
     @Transactional
-    public void updateBranch(String code, Branch branch){
-        this.branchRepository.save(branch);
+    public void updateBranch(String id, Branch branch) {
+        Boolean branchExists = this.branchRepository.existsById(id);
+        if (branchExists) {
+            Branch branchToUpdate = this.branchRepository.findById(id).get();
+            branchToUpdate.setName(branch.getName());
+            branchToUpdate.setPhoneNumber(branch.getPhoneNumber());
+            branchToUpdate.setAddress(branch.getAddress());
+            branchToUpdate.setBranchOfficeHours(branch.getBranchOfficeHours());
+            createOrUpdateBranch(branchToUpdate);
+        }
+        else {
+            throw new RuntimeException("The branch was not found");
+        }
     }
 
     @Transactional
-    public void deleteBranch(String code, Branch branch){
-        this.branchRepository.delete(branch);
+    public void deleteBranch(String id) {
+        Boolean branchExists = this.branchRepository.existsById(id);
+        if (branchExists)
+            this.branchRepository.deleteById(id);
+        else
+            throw new RuntimeException("The branch was not found");
+    }
+
+    private void createOrUpdateBranch(Branch branch) {
+        Boolean branchNameExists = this.branchRepository.existsByName(branch.getName());
+        Boolean branchAddressExists = this.branchRepository.existsByAddress(branch.getAddress());
+        if (branchNameExists || branchAddressExists)
+            throw new RuntimeException("There is already a branch named like this or in that location");
+        else
+            if (branch.getName().contains("Agencia"))
+                this.branchRepository.save(branch);
+            else
+                branch.setName("Agencia " + branch.getName());
+                this.branchRepository.save(branch);
     }
     
 }
