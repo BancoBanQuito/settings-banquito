32252c3d877e0ce0037de452539bb96dbbb27ee1
Nayeli Dayana Arellano Mena <84551223+ndarellano@users.noreply.github.com>
2023-01-15 16:45:45 -0500

    update model

--- settings/src/main/java/com/banquito/settings/model/BankEntity.java
+++ settings/src/main/java/com/banquito/settings/model/BankEntity.java
@@ -1,23 +1,27 @@
 package com.banquito.settings.model;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
+import java.util.List;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@Document(collection = "bankEntity")
+@Builder
+@Document(collection = "bank_entities")
 public class BankEntity {
+	
 	@Id
 	private String code;
 
-	@Field(value = "internationalBankCode")
 	private String internationalBankCode;
-
-	@Field(value = "name")
 	private String name;
 
+	private List<Branch> branches;
+
+	@Version
+	private Long version;
 }
