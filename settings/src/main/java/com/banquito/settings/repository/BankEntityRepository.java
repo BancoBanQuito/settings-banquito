0020e77dda5564bae81b795fb136f94d739e4fde
Nayeli Dayana Arellano Mena <84551223+ndarellano@users.noreply.github.com>
2023-01-15 18:15:06 -0500

    upload repository (#12)

--- settings/src/main/java/com/banquito/settings/repository/BankEntityRepository.java
+++ settings/src/main/java/com/banquito/settings/repository/BankEntityRepository.java
@@ -1,11 +1,7 @@
-package com.banquito.settings.repository;
-
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import com.banquito.settings.model.BankEntity;
 
-@Repository
-public interface BankEntityRepository extends CrudRepository<BankEntity, String> {
+public interface BankEntityRepository extends CrudRepository<BankEntity, String>{
 
 }
