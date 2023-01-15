0064dcd3a36cffd00254bd9472dbc308cae3eb0b
Jorge Galarza <jorge2000cruz@gmail.com>
2023-01-15 09:36:58 -0500

    Location Repository

--- settings/src/main/java/com/banquito/settings/repository/LocationRepository.java
+++ settings/src/main/java/com/banquito/settings/repository/LocationRepository.java
@@ -2,22 +2,16 @@ package com.banquito.settings.repository;
 
 import java.util.List;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.banquito.settings.model.Location;
 
-@Repository
-public interface LocationRepository extends MongoRepository<Location, String> {
+public interface LocationRepository extends CrudRepository<Location, String> {
 
-	List<Location> findByCountry(String country);
-
-	List<Location> findByProvince(String province);
+	List<Location> findByProvincia(String provincia);
 
 	List<Location> findByCanton(String canton);
 
-	List<Location> findByParish(String parish);
-
-	List<Location> findByCountryAndProvinceAndCantonAndParish(String country, String province, String canton,String parish);
+	List<Location> findByParroquia(String parroquia);
 
 }
