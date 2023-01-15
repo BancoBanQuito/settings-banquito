505ce809441018b85673b2447b064f0ed6cb67d7
Jorge Galarza <jorge2000cruz@gmail.com>
2023-01-15 09:14:52 -0500

    Change location model from A/Cs

--- settings/src/main/java/com/banquito/settings/model/Location.java
+++ settings/src/main/java/com/banquito/settings/model/Location.java
@@ -1,31 +1,23 @@
 package com.banquito.settings.model;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
-
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Builder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@NoArgsConstructor
-@Document(collection = "location")
+@Builder
+@Document(collection = "locations")
 public class Location {
 	@Id
-	private String code;
-
-	@Field(value = "provincia")
-	private String province;
-
-	@Field(value = "parroquia")
-	private String parish;
-
-	@Field(value = "canton")
+	private String id;
+	private String provincia;
+	private String parroquia;
+	@Indexed
 	private String canton;
-
-	@Field(value = "country")
-	private String country; // "Ecuador"
-
-	@Field(value = "postal_code")
-	private String postalCode;
+	private String codigoPostal;
+	@Version
+	private Long version;
 }
