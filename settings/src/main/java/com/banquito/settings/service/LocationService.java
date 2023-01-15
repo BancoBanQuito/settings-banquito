443f864697b4e63bb8107bdd8bff4fc142b416f6
Jorge Galarza <jorge2000cruz@gmail.com>
2023-01-15 09:43:06 -0500

    First Location Service

--- settings/src/main/java/com/banquito/settings/service/LocationService.java
+++ settings/src/main/java/com/banquito/settings/service/LocationService.java
@@ -1,57 +1,48 @@
 package com.banquito.settings.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.banquito.settings.model.Location;
 import com.banquito.settings.repository.LocationRepository;
 
 @Service
 public class LocationService {
+
 	private final LocationRepository locationRepository;
 
 	public LocationService(LocationRepository locationRepository) {
 		this.locationRepository = locationRepository;
 	}
 
-	public List<Location> findAll() {
-		return locationRepository.findAll();
-	}
-
-	public List<Location> findByCompleteLocation(String country, String province, String canton, String parish) {
-		return locationRepository.findByCountryAndProvinceAndCantonAndParish(country, province, canton, parish);
-	}
-
-	public List<Location> findByCountry(String country) {
-		return locationRepository.findByCountry(country);
-	}
-
-	public List<Location> findByProvince(String province) {
-		return locationRepository.findByProvince(province);
+	public List<Location> findByProvincia(String provincia) {
+		return locationRepository.findByProvincia(provincia);
 	}
 
 	public List<Location> findByCanton(String canton) {
 		return locationRepository.findByCanton(canton);
 	}
 
-	public List<Location> findByParish(String parish) {
-		return locationRepository.findByParish(parish);
+	public List<Location> findByParroquia(String parroquia) {
+		return locationRepository.findByParroquia(parroquia);
 	}
 
-	@Transactional
-	public void createLocation(Location location) {
-		this.locationRepository.save(location);
+	public Location findById(String id) {
+		Optional<Location> locationOpt = locationRepository.findById(id);
+		if (locationOpt.isPresent()) {
+			return locationOpt.get();
+		} else {
+			throw new RuntimeException("No se encontro la locacion");
+		}
 	}
 
-	@Transactional
-	public void updateLocation(String code, Location location) {
-		this.locationRepository.save(location);
+	public Location save(Location location) {
+		return locationRepository.save(location);
 	}
 
-	@Transactional
-	public void deleteLocation(String code, Location location) {
-		this.locationRepository.delete(location);
+	public void delete(Location location) {
+		locationRepository.delete(location);
 	}
 }
