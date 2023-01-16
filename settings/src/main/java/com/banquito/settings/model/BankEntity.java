bd4c979f2d9cf04f0039df3a0e2ea4dfe3bdb4f3
Nayeli Dayana Arellano Mena <84551223+ndarellano@users.noreply.github.com>
2023-01-15 20:48:35 -0500

    fixed change errors

--- settings/src/main/java/com/banquito/settings/model/BankEntity.java
+++ settings/src/main/java/com/banquito/settings/model/BankEntity.java
@@ -1,7 +1,5 @@
 package com.banquito.settings.model;
 
-import java.util.List;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Version;
 import org.springframework.data.mongodb.core.mapping.Document;
