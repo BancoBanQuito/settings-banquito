baea20d99be3fb433e132fc741cebc93c76f01fe
Carvajal Patiño Luis Enrique <44406616+carvajalluisweb@users.noreply.github.com>
2023-01-26 11:30:19 -0500

    Lc fix holiday (#24)
    * fix branch holiday
    
    * fix name in mapper
    
    * delete and post services
    
    * All services and controller added
    
    * fix holiday model
    
    * busqueda por codigo
    
    * fix update code
    
    * fix holiday service
    
    * fix comment

--- settings/src/main/java/com/banquito/settings/controller/mapper/HolidayMapper.java
+++ settings/src/main/java/com/banquito/settings/controller/mapper/HolidayMapper.java
@@ -13,24 +13,26 @@ public class HolidayMapper {
 
     public static Holiday toHoliday(HolidayRQ rq) throws ParseException {
         Date date = rq.getDate();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSSXXX");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String dateString = dateFormat.format(date);
-        
-        SimpleDateFormat dateFormat2 = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSSXXX");
+
+        SimpleDateFormat dateFormat2 = new SimpleDateFormat("yyyy-MM-dd");
         Date dateF = dateFormat2.parse(dateString);
 
         Calendar cal = Calendar.getInstance();
         cal.setTime(dateF);
-        
+
         cal.add(Calendar.DATE, 1);
-        cal.add(Calendar.HOUR, 5);
+        cal.add(Calendar.HOUR, 0);
         Date dateFinal = cal.getTime();
 
-        
-        return Holiday.builder().date(dateFinal).name(rq.getName()).codigoLocation(rq.getCode()).type(rq.getType()).build();
+        return Holiday.builder().date(dateFinal).name(rq.getName().toUpperCase()).codigoLocation(rq.getCode())
+                .type(rq.getType()).build();
+
     }
 
     public static HolidayRS toHolidayRS(Holiday holiday) {
-        return HolidayRS.builder().date(holiday.getDate()).code(holiday.getCodigoLocation()).name(holiday.getName()).type(holiday.getType()).build();
+        return HolidayRS.builder().date(holiday.getDate()).code(holiday.getCodigoLocation())
+                .name(holiday.getName().toUpperCase()).type(holiday.getType()).build();
     }
 }
